77307ead2355dc0a1f72cadbd56aa74ff013aa2b
jmgoya <jmgoya@gmail.com>
2022-10-16 13:46:45 -0300

    Meteoritos Destruyen Player

--- Autoloads/Eventos.gd
+++ Autoloads/Eventos.gd
@@ -4,4 +4,5 @@ extends Node
 
 signal disparo(proyectil)
 signal nave_destruida(posicion, explosiones)
-
+signal spawn_meteorito (posicion, direccion, tamanio)
+signal destruccion_meteorito (posicion)

92ae4076a535c0bc469fbd4ca2258bc02ea527e4
jmgoya <jmgoya@gmail.com>
2022-10-24 18:55:13 -0300

    Estacion

---
+++ Juego/Estaciones/EstracionRecarga.gd
@@ -0,0 +1,56 @@
+# EstacionRecarga.gd
+class_name EstacioRecarga
+extends Node2D
+
+## Atributos Export
+export var energia:float = 10
+export var radio_energia_entregada:float = 0.5
+
+onready var carga_sfx:AudioStreamPlayer = $AudioCarga
+# Atributos
+var nave_player:Player = null
+var player_en_zona:bool = false
+
+# Metodos
+func _unhandled_input(event: InputEvent) -> void:
+	if not puede_recargar(event):
+		return
+	
+	controlar_energia()
+	
+	if event.is_action("recargar_escudo"):
+		nave_player.get_escudo().controlar_energia(radio_energia_entregada)
+	if event.is_action("recargar_rayo"):
+		nave_player.get_laser().controlar_energia(radio_energia_entregada)
+
+# Metodos Custom
+func puede_recargar(event: InputEvent) -> bool:
+	var hay_input = event.is_action("recargar_escudo") or event.is_action("recargar_rayo")
+	if hay_input and player_en_zona and energia > 0.0:
+		if !carga_sfx.playing:
+			carga_sfx.play()
+		return true
+	return false
+
+func controlar_energia() -> void:
+	print (energia)
+	energia -= radio_energia_entregada
+	if energia <= 0.0:
+		$AudioVacio.play()
+	print ("Energia Estacion ", energia)
+
+# Señales internas
+func _on_AreaRecarga_body_entered(body: Node) -> void:
+	if body is Player:
+		nave_player = body
+		player_en_zona = true
+	body.set_gravity_scale(0.1)
+
+func _on_AreaRecarga_body_exited(body: Node) -> void:
+	if body is Player:
+		player_en_zona = false
+	body.set_gravity_scale(0.0)
+
+func _on_AreaColision_body_entered(body: Node) -> void:
+	if body.has_method("destruir"):
+		body.destruir()
--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -19,15 +19,22 @@ var estado_actual:int = ESTADOS.SPAWN
 
 ## Atributos onready
 onready var canion:Canion = $Canion
-onready var laser:RayoLaser = $LaserBeam2D
+onready var laser:RayoLaser = $LaserBeam2D setget ,get_laser
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
 onready var colisionador:CollisionShape2D = $CollisionShape2D
 onready var impacto_sfx:AudioStreamPlayer = $ImpactosSFX
-onready var escudo:Escudo = $Escudo
+onready var escudo:Escudo = $Escudo setget ,get_escudo
 
 #JMG
 onready var animacion:AnimationPlayer = $AnimationPlayer
+
+## Setter y Getters
+func get_laser() -> RayoLaser:
+	return laser
+
+func get_escudo() -> Escudo:
+	return escudo
 
 ## Metodos
 func _ready() -> void:

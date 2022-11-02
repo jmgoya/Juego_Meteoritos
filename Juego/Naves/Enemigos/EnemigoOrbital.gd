1455115dc270a6551afa72b430fdf98e68881e90
jmgoya <jmgoya@gmail.com>
2022-11-02 18:06:30 -0300

    Final

--- Juego/Naves/Enemigos/EnemigoOrbital.gd
+++ Juego/Naves/Enemigos/EnemigoOrbital.gd
@@ -4,14 +4,23 @@ extends EnemigosBase
 
 ## Atributos
 var base_duenia:Node2D
+var ruta:Path2D
+var path_follow:PathFollow2D
 
 ## Atributos Export
 export var rango_max_ataque: float = 800.0
+export var velocidad:float = 200.0
+
+## Atributo On Ready
+onready var detector_obstaculo:RayCast2D = $DetectorObstaculo
 
 #constructor
-func crear(pos: Vector2, duenia: Node2D) -> void:
+func crear(pos: Vector2, duenia: Node2D, ruta_duenia: Path2D) -> void:
 	global_position = pos
 	base_duenia = duenia
+	ruta = ruta_duenia
+	path_follow = PathFollow2D.new()
+	ruta.add_child(path_follow)
 
 #Metodos
 func _ready() -> void:
@@ -19,10 +28,14 @@ func _ready() -> void:
 	#Temporal
 	canion.set_esta_disparando(true)
 
+func _process(delta: float) -> void:
+	path_follow.offset += velocidad * delta
+	position = path_follow.global_position
+
 #Metodos Custom
 func rotar_hacia_player() -> void:
 	.rotar_hacia_player()
-	if dir_player.length() > rango_max_ataque:
+	if dir_player.length() > rango_max_ataque or not detector_obstaculo.is_colliding():
 		canion.set_esta_disparando(false)
 	else:
 		canion.set_esta_disparando(true)
